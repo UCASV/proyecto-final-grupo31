00a98cf37a59dd68ba8c26005cab6305bcec3465
KevinEsc21 <00116120@uca.edu.sv>
2021-06-26 19:12:35 -0600

    Adding login exception

--- Proyecto BASES-POO 2021/Programación orientada a objetos/ProyectoPOOxBDD/View/frmLogIn.cs
+++ Proyecto BASES-POO 2021/Programación orientada a objetos/ProyectoPOOxBDD/View/frmLogIn.cs
@@ -21,67 +21,74 @@ namespace ProyectoPOOxBDD
 
         private void btnLogIn_Click(object sender, EventArgs e)
         {
-            //Obteniendo todos los gestores
-            var db = new VaccinationDBContext();
-
-            List<Manager> managers = db.Managers
-                                    .OrderBy(m => m.Id)
-                                    .ToList();
-
-            //Filtrar datos
-            string userName = txtUser.Text;
-
-            string userPass = txtPassword.Text;
-
-            List<Manager> result = managers
-                                    .Where(m => m.Username == userName)
-                                    .ToList();
-
-            //Enlazar datos al siguiente formulario
-
-            if (userName.Length > 0 && userPass.Length > 0)
+            try
             {
-                if (result.Count() > 0)
+                //Obteniendo todos los gestores
+                var db = new VaccinationDBContext();
+
+                List<Manager> managers = db.Managers
+                                        .OrderBy(m => m.Id)
+                                        .ToList();
+
+                //Filtrar datos
+                string userName = txtUser.Text;
+
+                string userPass = txtPassword.Text;
+
+                List<Manager> result = managers
+                                        .Where(m => m.Username == userName)
+                                        .ToList();
+
+                //Enlazar datos al siguiente formulario
+
+                if (userName.Length > 0 && userPass.Length > 0)
                 {
-                    if (result[0].KeyCode == userPass)
+                    if (result.Count() > 0)
                     {
-                        //Agregar el inicio de sesión al historial
-                        LogInHistory login = new LogInHistory()
+                        if (result[0].KeyCode == userPass)
                         {
-                            IdManager = result[0].Id, 
-                            LogInDateTime = DateTime.Now
-                        };
+                            //Agregar el inicio de sesión al historial
+                            LogInHistory login = new LogInHistory()
+                            {
+                                IdManager = result[0].Id,
+                                LogInDateTime = DateTime.Now
+                            };
 
-                        db.Add(login);
-                        db.SaveChanges();
+                            db.Add(login);
+                            db.SaveChanges();
 
-                        //Instanciar formulario que aun no hemos creado
-                        frmPrincipal principal = new frmPrincipal(result[0]);
+                            //Instanciar formulario que aun no hemos creado
+                            frmPrincipal principal = new frmPrincipal(result[0]);
 
-                        //Ocultar formulario actual
-                        this.Hide();
+                            //Ocultar formulario actual
+                            this.Hide();
 
-                        //Mostrar formulario que aun no tenemos
-                        principal.ShowDialog();
+                            //Mostrar formulario que aun no tenemos
+                            principal.ShowDialog();
 
-                        //Reestablecer textbox
-                        txtUser.Text = String.Empty;
-                        txtPassword.Text = String.Empty;
-                        this.Show();
+                            //Reestablecer textbox
+                            txtUser.Text = String.Empty;
+                            txtPassword.Text = String.Empty;
+                            this.Show();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Contraseña incorrecta", "Vacunación Covid-19", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
                     }
                     else
                     {
-                        MessageBox.Show("Contraseña incorrecta", "Vacunación Covid-19", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        MessageBox.Show("Usuario no existente", "Vacunación Covid-19", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     }
                 }
                 else
                 {
-                    MessageBox.Show("Usuario no existente", "Vacunación Covid-19", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    MessageBox.Show("Debe llenar todos los campos", "Vacunación Covid-19", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 }
             }
-            else
+            catch (Exception exception)
             {
-                MessageBox.Show("Debe llenar todos los campos", "Vacunación Covid-19", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBox.Show("Error al conectar con la base de datos", "Vacunación Covid-19", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
         }
     }
